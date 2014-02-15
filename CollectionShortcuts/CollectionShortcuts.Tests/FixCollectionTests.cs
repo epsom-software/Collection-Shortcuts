a42fb9aeaad0250af2e83b126de4c98cad8f883c
buhbuh <JuggleDrum@hotmail.co.uk>
2014-02-15 16:54:50 +0000

    Empty solution

---
+++ CollectionShortcuts/CollectionShortcuts.Tests/FixCollectionTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CollectionShortcuts.Tests
+{
+    [TestClass]
+    public class FixCollectionTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
