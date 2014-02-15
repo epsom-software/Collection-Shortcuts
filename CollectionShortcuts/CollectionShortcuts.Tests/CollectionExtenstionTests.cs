061b2c8e3062dcd78c0b3c329b02f7b195e41ec4
buhbuh <JuggleDrum@hotmail.co.uk>
2014-02-15 20:49:12 +0000

    Shortcut extension method

---
+++ CollectionShortcuts/CollectionShortcuts.Tests/CollectionExtenstionTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace CollectionShortcuts.Tests
+{
+    [TestClass]
+    public class CollectionExtenstionTests
+    {
+        [TestMethod]
+        public void CanConvertTargetToFixCollection()
+        {
+            ICollection<AnnoyingPairs.NamespaceA.NameValuePair> target = new Collection<AnnoyingPairs.NamespaceA.NameValuePair>
+            {
+                new AnnoyingPairs.NamespaceA.NameValuePair
+                {
+                    Key = "key0", 
+                    Value="value0"
+                }
+            };
+
+            FixCollection<AnnoyingPairs.NamespaceA.NameValuePair> result = target.Fix();
+            Assert.IsNotNull(result);
+        }
+    }
+}
---
+++ CollectionShortcuts/CollectionShortcuts/CollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionShortcuts
+{
+    public static class CollectionExtensions
+    {
+        public static FixCollection<T> Fix<T>(this ICollection<T> source) where T : new()
+        {
+            return new FixCollection<T>(source);
+        }
+    }
+}
